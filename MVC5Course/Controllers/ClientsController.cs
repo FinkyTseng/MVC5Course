b709804218f3a794b3986cee65a1aee1b3a46e0e
FinkyTseng <finkytiger@gmail.com>
2016-03-19 15:56:24 +0800

    將 /Client/Index 頁面透過 PagedList.Mvc 加上分頁功能與分頁導覽列

--- MVC5Course/Controllers/ClientsController.cs
+++ MVC5Course/Controllers/ClientsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using MVC5Course.Models;
+using PagedList;
 
 namespace MVC5Course.Controllers
 {
@@ -15,10 +16,10 @@ namespace MVC5Course.Controllers
         private FabricsEntities db = new FabricsEntities();
 
         // GET: Clients
-        public ActionResult Index()
+        public ActionResult Index(int pageNo = 1)
         {
-            var client = db.Client.Include(c => c.Occupation);
-            return View(client.Take(10));
+            var client = db.Client.Include(c => c.Occupation).OrderBy(p=>p.ClientId);
+            return View(client.ToPagedList(pageNo,10));
         }
 
         // GET: Clients/Details/5
@@ -88,8 +89,7 @@ namespace MVC5Course.Controllers
             {
                 db.Entry(client).State = EntityState.Modified;
                 db.SaveChanges();
-
-                return View("Index", db.Client.Include(c => c.Occupation).Take(10));
+                return View("Index", db.Client.Include(c => c.Occupation).ToList());
             }
             ViewBag.OccupationId = new SelectList(db.Occupation, "OccupationId", "OccupationName", client.OccupationId);
             return View(client);
