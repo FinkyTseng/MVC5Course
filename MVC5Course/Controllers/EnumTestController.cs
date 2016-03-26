30f96ce9315bbc12708d5550644dd18d14e3e0b4
FinkyTseng <finkytiger@gmail.com>
2016-03-26 17:15:03 +0800

    利用 EnumHelper 輸出 enum 型別的屬性

---
+++ MVC5Course/Controllers/EnumTestController.cs
@@ -0,0 +1,42 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class EnumTestController : Controller
+    {
+        // GET: EnumTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MyEnum()
+        {
+            ViewData.Model = new EnumViewModel()
+            {
+                id = 1,
+                name = "Finky",
+                status = status.A
+            };
+
+            return View();
+        }
+
+        public ActionResult MyEnumView()
+        {
+            ViewData.Model = new EnumViewModel()
+            {
+                id = 1,
+                name = "Finky",
+                status = status.A
+            };
+
+            return View();
+        }
+    }
+}
---
+++ MVC5Course/Models/EnumViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models
+{
+    public enum status
+    {
+        A,
+        B,
+        C
+    };
+
+    public class EnumViewModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public status status { get; set; }
+    }
+}
